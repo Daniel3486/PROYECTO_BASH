b4ef4442a15b4be7ddd32efc23894825f5d6a7e0
AXEL RICARDO MONCLOA MURO <a348752@uach.mx>
2023-03-13 22:59:04 -0600

    se agrega la segunda parte del codigo en bash

--- bash.sh
+++ bash.sh
@@ -5,66 +5,105 @@ then
 	echo "Debes de pasar un parámetro"
 elif [[ "$1" = "-a" || "$1" = "-t" ]]
 then
-	if [[ "$1" = "-a" ]]
-	then
-		echo -e "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:\n"
-		echo -e "\t1. SCRUM"
-		echo -e "\t2. X.P."
-    		echo -e "\t3. Kanban"
-	        echo -e "\t4. Crystal\n"
-			read -p "Tema: " tema
-			case $tema in
-			1)
-				seccion="SCRUM"
-				;;
+	while true; do
+		if [[ "$1" = "-a" ]]
+		then			
+			echo -e "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:\n"
+			echo -e "\t1. SCRUM"
+			echo -e "\t2. X.P."
+        		echo -e "\t3. Kanban"
+		        echo -e "\t4. Crystal\n"
+			tema=" "
+			while [[ "$tema" -ne "1" && "$tema" -ne "2" && "$tema" -ne "3" && "$tema" -ne "4" ]]; do
+				read -p "Tema: " tema
+				case $tema in
+				1)
+					seccion="SCRUM"
+					archivo="SCRUM.inf"
+					;;
+				2)
+					seccion="X.P."
+					archivo="XP.inf"
+					;;
+				3)
+					seccion="Kanban"
+					archivo="Kanban.inf"
+					;;
+				4)	
+					seccion="Crystal"
+					archivo="Crystal.inf"
+					;;
+				*)
+					echo "Opción inválida"
+					;;
+				esac
+			done
+		elif [[ "$1" = "-t" ]]
+		then	
+			echo -e "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:\n"
+			echo -e "\t1. Cascada"
+		        echo -e "\t2. Espiral"
+        		echo -e "\t3. Modelo V\n"
+			tema=" "
+	        	while [[ "$tema" -ne "1" && "$tema" -ne "2" && "$tema" -ne "3" ]]; do
+        	        	read -p "Tema: " tema
+	        	        case $tema in
+        	        	1)
+                	        	seccion="Cascada"
+					archivo="Cascada.inf"
+        	                	;;
+	        	        2)
+        	        	        seccion="Espiral"
+					archivo="Espiral.inf"
+	                	        ;;
+		                3)
+        		                seccion="Modelo V"
+					archivo="Modelo_V.inf"
+	                	        ;;
+	                	esac
+        		done
+		fi
+		echo ""
+		opcion=" "
+		while [[ "$opcion" -ne "5" && "$opcion" -ne "6" ]]; do		
+			echo -e "Usted esta en la sección $seccion, selecione la opción que desea utilizar.\n"
+		        echo -e "\t1. Agregar información"
+        		echo -e "\t2. Buscar"
+		        echo -e "\t3. Eliminar información"
+			echo -e "\t4. Leer base de información"
+			echo -e "\t5. Menú anterior"
+			echo -e "\t6. Terminar ejecución\n"
+			read -p "Opción: " opcion
+                	echo ""
+			case $opcion in
+                	1)
+               			read -p "Concepto: " concepto
+                               	read -p "Definicion: " definicion
+		         	echo "[$concepto] .- $definicion." >> "$archivo"
+        	                echo -e "\nConcepto agregado exitosamente.\n"
+                	        ;;
 			2)
-				seccion="X.P."
+				:
 				;;
 			3)
-				seccion="Kanban"
+				:
 				;;
 			4)
-				seccion="Crystal"
+				:
 				;;
-			*)
-				echo "Opción inválida"
+			5)
+				:
 				;;
-			esac
-	elif [[ "$1" = "-t" ]]
-	then
-		echo -e "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:\n"
-		echo -e "\t1. Cascada"
-	        echo -e "\t2. Espiral"
-    		echo -e "\t3. Modelo V\n"
-    	        	read -p "Tema: " tema
-        	        case $tema in
-    	        	1)
-            	        	seccion="Cascada"
-    	                	;;
-        	        2)
-    	        	        seccion="Espiral"
-                	        ;;
-	                3)
-    		                seccion="Modelo V"
-                	        ;;
-	                *)
-    		                echo "Opción inválida"
-            		        ;;
-                	esac
-	fi
-	echo ""
-	opcion=" "
-	while [[ "$opcion" -ne "6" ]]; do
-		echo -e "Usted esta en la sección $seccion, selecione la opción que desea utilizar.\n"
-	        echo -e "\t1. Agregar información"
-    		echo -e "\t2. Buscar"
-	        echo -e "\t3. Eliminar información"
-		echo -e "\t4. Leer base de información"
-		echo -e "\t5. Menú anterior"
-		echo -e "\t6. Terminar ejecución\n"
-		read -p "Opción: " opcion
-            	echo ""
+			6)
+				exit 0
+				;;
+               	        *)
+                       	        echo -e "Opción invalida\n"
+                               	;;
+	                esac
+		done
 	done
 else
 	echo "Parámetro no valido"
-fi
+fi						
+
